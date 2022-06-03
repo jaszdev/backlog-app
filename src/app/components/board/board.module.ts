33acb27e70725918a88dc4a310454bed56f4f3bb
josue <josvillalobos@mobilize.net>
2022-06-03 09:23:13 -0600

    Adding board.module.ts

---
+++ src/app/components/board/board.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BoardComponent } from './board/board.component';
+
+
+
+@NgModule({
+  declarations: [
+    BoardComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  export: [
+    BoardComponent
+  ]
+})
+export class BoardModule { }
